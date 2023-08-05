a420531b1eb889adaddfad977c51682b9820fff2
Reinhard Chemonges <chemorein24@gmail.com>
2023-08-05 15:05:05 +0300

    first commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="container">
+<hr>
+<p class="centered">PHONE BILLING</b></p>
+</div>
---
+++ user/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+        PHONE BILLING SYSTEM
+            </div>
+        </div>
+    </footer>
